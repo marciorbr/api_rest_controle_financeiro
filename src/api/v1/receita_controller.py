45f945271aac3e9d96fa25a2591ac3a36f095fd6
Márcio Oliveira <marcio035@gmail.com>
2024-01-29 12:02:16 -0400

    Adicionando validação para não inserir receita com descrição já cadastrada

--- src/api/v1/receita_controller.py
+++ src/api/v1/receita_controller.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from typing import Annotated
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from src.database import get_session
@@ -13,6 +14,12 @@ Session = Annotated[Session, Depends(get_session)]
 
 @router.post('/', response_model=ReceitaPublic )
 def criar_receita(receita: ReceitaSchema, session: Session):
+    receita_descricao = session.scalar(select(Receitas).where(Receitas.descricao == receita.descricao))
+    
+    if receita_descricao:
+        raise HTTPException(status_code=400, detail='Essa descição de receita já existe')
+
+
     new_receita: Receitas = Receitas(
         descricao=receita.descricao,
         valor=receita.valor,
@@ -23,3 +30,4 @@ def criar_receita(receita: ReceitaSchema, session: Session):
     session.refresh(new_receita)
 
     return new_receita
+
